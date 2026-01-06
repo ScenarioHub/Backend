569695a0b385d5448ff9a4fb808baa1f54fd6cd9
fhddhdy37 <fhddhdy37@tukorea.ac.kr>
2026-01-06 21:32:29 +0900

    Feat: Add scenario post API

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import scenario_detail
+from .views import scenario_detail, upload_post
 
 urlpatterns = [
     path('scenario-detail/<int:id>/', scenario_detail),
+    path('upload/post/', upload_post),
 ]
--- scenariohub_backend/settings.py
+++ scenariohub_backend/settings.py
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+DATA_ROOT = BASE_DIR.parent / 'data'
