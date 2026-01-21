25f4a1ff19cfd171d43a86ae8ce1226f53afdf34
fhddhdy37 <fhddhdy37@naver.com>
2026-01-21 16:25:57 +0900

    Refactor: Rename API
    /api/scenario-detail/{id}/ => /api/scenarios/{id}/details/

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,9 @@ from .views import register, login, post_list, scenario_detail, upload_post, dow
 urlpatterns = [
     path('auth/register/', register),
     path('auth/login/', login),
-    path('scenarios/explore/', post_list),
-    path('scenario-detail/<int:id>/', scenario_detail),
     path('upload/post/', upload_post),
+    path('scenarios/explore/', post_list),
+    path('scenarios/<int:id>/details/', scenario_detail),
     path('scenarios/<int:id>/download/', download_file, name='scenario_download'),
     path('scenarios/<int:id>/video/', stream_video, name='scenario-video'),
 ]
