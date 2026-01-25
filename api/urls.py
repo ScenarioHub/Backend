c00907086fcd6bf875928b4dd82caf57c9f81eed
cjswkans2002 <cjswkans2002@gmail.com>
2026-01-25 22:38:00 +0900

    Fix: urls 수정
    1. 관련 api별 순서 수정
    
    2. explore(목록 조회 api)의 query parameter 사용 편의성을 위한 path 수정:
    path('scenarios/explore/', ...) → path('scenarios/explore'', ...)

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from .views import register, login, post_list, scenario_detail, upload_post, xosc_download, stream_video
 
 urlpatterns = [
+    # 인증 관련
     path('auth/register/', register),
     path('auth/login/', login),
-    path('posts/', post_list),
-    path('scenario-detail/<int:id>/', scenario_detail),
+    # 시나리오 관련
+    path('scenarios/explore', post_list, name='scenarios_explore_list'),
+    path('scenarios/<int:id>/details/', scenario_detail, name='scenarios_details_list'),
+    path('scenarios/<int:id>/download/', xosc_download.download_file, name='scenarios_download_list'),
+    path('scenarios/<int:id>/video/', stream_video, name='scenarios_video_list'),
+    # 업로드
     path('upload/post/', upload_post),
-    path('scenarios/<int:id>/download/', xosc_download.download_file, name='scenario_download'),
-    path('scenarios/<int:id>/video/', stream_video, name='scenario-video'),
 ]
