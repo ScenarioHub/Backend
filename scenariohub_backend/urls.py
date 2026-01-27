f8d16ac5096ad4f3d438550d72f6be74b4c606fe
fhddhdy37 <fhddhdy37@naver.com>
2026-01-27 20:18:58 +0900

    Fix: Fix CORS error
    CORS 에러 수정

--- scenariohub_backend/urls.py
+++ scenariohub_backend/urls.py
@@ -29,7 +29,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
-    url='https://scenariohub.iptime.org',
+    url='http://scenariohub.iptime.org',
 )
 
 urlpatterns = [
